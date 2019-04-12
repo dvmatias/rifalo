271088fb21dc42cb616ca0f6ef901d80cb5845f3
Matías <matias.delv.dom@gmail.com>
2019-04-12 00:00:09 -0300

    Splash Activity:
    Added background image resource. Background painted below status bar.

--- app/src/main/java/com/matias/rifalo/SplashActivity.kt
+++ app/src/main/java/com/matias/rifalo/SplashActivity.kt
@@ -2,11 +2,17 @@ package com.matias.rifalo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SplashActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_splash)
+
+		// Draw below status bar, android:statusBarColor attribute must be transparent.
+		window.decorView.systemUiVisibility =
+			View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
 	}
 }
