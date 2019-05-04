0a72b000e9d51fd0b835ea0f48d4d1d986e3d575
Matías <matias.delv.dom@gmail.com>
2019-05-04 15:13:05 -0300

    Loading fragment now extends custom dialog class.

--- app/src/main/java/com/bluespark/raffleit/common/views/dialogs/LoadingDialogFragment.kt
+++ app/src/main/java/com/bluespark/raffleit/common/views/dialogs/LoadingDialogFragment.kt
@@ -19,7 +19,7 @@ import kotlinx.android.synthetic.main.fragment_loading_dialog.*
  * TODO dec.
  */
 
-class LoadingDialogFragment : DialogFragment() {
+class LoadingDialogFragment : MyDialogFragment() {
 
 	companion object {
 		@JvmStatic
@@ -46,19 +46,13 @@ class LoadingDialogFragment : DialogFragment() {
 
 	override fun onResume() {
 		super.onResume()
+		isCancelable = false
 		applyImmersiveFullScreen()
 
-		val wm =
-			context?.applicationContext?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-		val display = wm.defaultDisplay
-		val metrics = DisplayMetrics()
-		display.getMetrics(metrics)
-		val widthDisplay = metrics.widthPixels
-		val width = widthDisplay * 0.8
-		val height = LinearLayout.LayoutParams.WRAP_CONTENT
-		dialog.window!!.setLayout(width.toInt(), height)
-		dialog.window!!.setBackgroundDrawableResource(R.drawable.shpae_dialog_fragment_bgr)
+		animateLoadingImage()
+	}
 
+	private fun animateLoadingImage() {
 		val rotate = RotateAnimation(
 			0f,
 			360f,
@@ -74,20 +68,5 @@ class LoadingDialogFragment : DialogFragment() {
 		iv_loading.startAnimation(rotate)
 
 		dialog.setCancelable(false)
-	}
-
-	private fun applyImmersiveFullScreen() {
-		// Draw below status bar, android:statusBarColor attribute must be transparent.
-		// Hide navigation bar.
-		dialog.window?.decorView!!.systemUiVisibility =
-			View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-					View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-					View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-					View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-					View.SYSTEM_UI_FLAG_IMMERSIVE
-
-//		dialog.window?.decorView!!.setOnSystemUiVisibilityChangeListener {
-//			checkSystemUiVisibility(it)
-//		}
 	}
 }
